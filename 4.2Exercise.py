9d1b0daf3577edccbb98fc640c4ecd24c27a818d
DillTheBeast <dillonmaltese@gmail.com>
2023-03-02 13:26:03 -0500

    Create 4.2Exercise.py

---
+++ 4.2Exercise.py
@@ -0,0 +1,11 @@
+
+
+#Dillon Maltese
+#3/2/23
+#Exercise 4.2
+
+print("This is not my first real program!")
+#Python is a super cool
+#and fun programming language
+
+print('\tThis makes a tab \nThis makes a new line \n also the single quotes and double quotes both work too')
