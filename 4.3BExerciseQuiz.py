d4b21897ec2c1a5c69ee2d5788d93e657d22b95a
DillTheBeast <dillonmaltese@gmail.com>
2023-03-06 14:50:47 -0500

    Update 4.3BExerciseQuiz.py

--- 4.3BExerciseQuiz.py
+++ 4.3BExerciseQuiz.py
@@ -1,7 +1,8 @@
-#Question 1
+#Dillon Maltese
+#4.3B Part 3
 
 answer1 = input("Binary is only made of 1's and 0's. True or False: ")
-if(answer1 == "true"):
+if(answer1 == "true" or answer1 == "True"):
     print("You are correct")
 else:
-    print()
+    print("You are wrong")
